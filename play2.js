cdd4e426a63dbedec8eecadf22e5677227c3df29
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-04-29 15:47:04 +0700

    [Fresly] - commit from folder 2 video 9. Understanding Spread & Rest Operators

--- play2.js
+++ play2.js
@@ -11,6 +11,9 @@ const person = {
   },
 };
 
+const copiedPerson = { ...person };
+console.log(copiedPerson);
+
 const hobbies = ["Sports", "Cooking"];
 // for (let hobby of hobbies) {
 //   console.log(hobby);
@@ -19,5 +22,21 @@ const hobbies = ["Sports", "Cooking"];
 
 // console.log(hobbies);
 
-hobbies.push("Programming");
-console.log(hobbies);
+// copy array
+const copiedArray = hobbies.slice();
+
+// not copy array, it's create array on array
+// const copiedArray = [hobbies];
+
+// copy with spread operator
+const copiedArraySpread = [...hobbies];
+console.log(copiedArray);
+console.log(copiedArraySpread);
+
+// rest operator
+// const toArray = (arg1, arg2, arg3) => {
+const toArray = (...args) => {
+  return args;
+};
+
+console.log(toArray(1, 2, 3, 4));
