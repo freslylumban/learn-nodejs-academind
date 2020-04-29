1e16320935a482c661757191b8b7de83841f5999
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-04-29 14:34:56 +0700

    [Fresly] - commit from folder 2 video 5. Understanding Arrow Functions

--- play.js
+++ play.js
@@ -4,7 +4,7 @@ const hasHobbies = true;
 
 age = 30;
 
-function summerizeUser(userName, userAge, userHasHobby) {
+const summerizeUser = (userName, userAge, userHasHobby) => {
   return (
     "Name is " +
     userName +
@@ -13,6 +13,18 @@ function summerizeUser(userName, userAge, userHasHobby) {
     " and the user has hobbies " +
     userHasHobby
   );
-}
+};
+
+const add = (a, b) => a + b;
+
+const addOne = (a) => a + 1;
+
+const addRandom = () => 5 + 6;
 
 console.log(summerizeUser(name, age, hasHobbies));
+
+console.log(add(1, 2));
+
+console.log(addOne(3));
+
+console.log(addRandom());
