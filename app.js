4d817c2d4c66f3f600d19f0f55a5e0c9b0ba1c21
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-05-04 20:52:06 +0700

    [Fresly] - commit from folder 3 video 9. Routing Requests

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 const http = require("http");
 
-// function rqListener(req, res) {}
-
-// CALL FUNCTION
-// http.createServer(rqListener);
-
-// OLD JAVASCRIPT
-// http.createServer(function (req, res) {});
-
-// NEXT-GEN JAVASCRIPT
-// http.createServer((req, res) => {});
-
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  // process.exit();
-
+  const url = req.url;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title></head>');
