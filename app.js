dde6ffb7c267bb37ba7a7979505f8918d7e7cd9b
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-05-07 01:51:34 +0700

    [Fresly] - commit from folder 5 video 16. Returning a 404 Page

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -9,10 +11,10 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(shopRoutes);
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 
 app.use((req, res, next) => {
-	res.status(404).send("<h1>Page not found</h1>");
+	res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(8000);
