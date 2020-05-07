d16a62df95193c254385b580a0af0ba069199464
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-05-08 02:58:16 +0700

    [Fresly] - commit from folder 6 video 14. Working with EJS

--- app.js
+++ app.js
@@ -2,19 +2,26 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+// FOR VIEWS ENGINE HANDLEBARS
 const expressHandlebars = require('express-handlebars');
 
 const app = express();
 
-app.engine(
-	'handlebars',
-	expressHandlebars({
-		layoutsDir: 'views/layouts',
-		defaultLayout: 'main-layout',
-	})
-);
-app.set('view engine', 'handlebars');
+// HANDLEBARS
+// app.engine(
+// 	'handlebars',
+// 	expressHandlebars({
+// 		layoutsDir: 'views/layouts',
+// 		defaultLayout: 'main-layout',
+// 	})
+// );
+// app.set('view engine', 'handlebars');
+
+// PUG (JADE)
 // app.set('view engine', 'pug');
+
+// EJS
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
