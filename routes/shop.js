c827aee3ccce2ce117dd824f101cfd5de8abb040
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-05-08 01:34:36 +0700

    [Fresly] - commit from folder 6 video 9. Finishing the Pug Template

--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,15 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
+const express = require('express');
 
-const rootDir = require("../utils/path");
-const adminData = require("./admin");
+const rootDir = require('../utils/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
+router.get('/', (req, res, next) => {
 	const products = adminData.products;
-	res.render("shop", { prods: products, docTitle: 'Shop' });
+	res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' });
 });
 
 module.exports = router;
