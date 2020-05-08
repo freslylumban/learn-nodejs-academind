3b241c052fd752a463fd605302a2ac315e5f6563
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-05-08 14:07:42 +0700

    [Fresly] - commit from folder 7 video 4. Finishing the Controllers

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
 // FOR VIEWS ENGINE HANDLEBARS
-const expressHandlebars = require('express-handlebars');
+// const expressHandlebars = require('express-handlebars');
 
 const app = express();
 
@@ -33,8 +36,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-	res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.get404);
 
 app.listen(8000);
