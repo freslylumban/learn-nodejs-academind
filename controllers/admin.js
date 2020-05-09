13051349fd72684ba92e2eb433ab1cbcab004cc8
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-05-09 12:23:16 +0700

    [Fresly] - commit from folder 9 video 10. Using Query Params

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,21 +4,30 @@ exports.getAddProduct = (req, res, next) => {
 	res.render('admin/add-product', {
 		pageTitle: 'Add Product',
 		path: '/admin/add-product',
-		activeAddProduct: true,
-		formCSS: true,
-		productCSS: true,
 	});
 };
 
 exports.postAddProduct = (req, res, next) => {
-  console.log(req.body);
-  const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
-  const price = req.body.price;
-  const description = req.body.description;
+	console.log(req.body);
+	const title = req.body.title;
+	const imageUrl = req.body.imageUrl;
+	const price = req.body.price;
+	const description = req.body.description;
 	const product = new Product(title, imageUrl, description, price);
 	product.save();
 	res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+	const editMode = req.query.edit;
+	if (!editMode) {
+		// return res.redirect('/');
+	}
+	res.render('admin/edit-product', {
+		pageTitle: 'Edit Product',
+		path: '/admin/edit-product',
+		editing: editMode,
+	});
 };
 
 exports.getProducts = (req, res, next) => {
