157931f7c242c61b903638d5728c3ffc15d43b7d
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-05-12 01:55:45 +0700

    [Fresly] - commit from folder 10 video 6. Connecting our App to the SQL Database

--- app.js
+++ app.js
@@ -4,31 +4,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-
-// FOR VIEWS ENGINE HANDLEBARS
-// const expressHandlebars = require('express-handlebars');
+const db = requier('./utils/database');
 
 const app = express();
 
-// HANDLEBARS
-// app.engine(
-// 	'handlebars',
-// 	expressHandlebars({
-// 		layoutsDir: 'views/layouts',
-// 		defaultLayout: 'main-layout',
-// 	})
-// );
-// app.set('view engine', 'handlebars');
-
-// PUG (JADE)
 app.set('view engine', 'pug');
-
-// EJS
-// app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('SELECT * FROM products')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
