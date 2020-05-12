a4c82b24f96fd3058dd0d7e1d6a867f8a9710311
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-05-12 17:47:48 +0700

    [Fresly] - commit from folder 11 video 6. Inserting Data & Creating a Product

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,16 +14,19 @@ exports.postAddProduct = (req, res, next) => {
 	const imageUrl = req.body.imageUrl;
 	const price = req.body.price;
 	const description = req.body.description;
-	const product = new Product(null, title, imageUrl, description, price);
-	product
-		.save()
-		.then(() => {
-			res.redirect('/');
+	Product.create({
+		title: title,
+		price: price,
+		imageUrl: imageUrl,
+		description: description,
+	})
+		.then((result) => {
+			// console.log(result);
+			console.log('Product created!');
 		})
 		.catch((err) => {
 			console.log(err);
 		});
-	res.redirect('/');
 };
 
 exports.getEditProduct = (req, res, next) => {
