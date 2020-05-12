6b8a790e0f413b170d958acde3bd41890581b88c
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-05-12 17:25:24 +0700

    [Fresly] - commit from folder 11 video 5. Syncing JS Definitions to the Database

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./utils/database');
+const sequelize = require('./utils/database');
 
 const app = express();
 
@@ -22,4 +22,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(8000);
+sequelize
+	.sync()
+	.then((result) => {
+		console.log(result);
+		app.listen(8000);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
