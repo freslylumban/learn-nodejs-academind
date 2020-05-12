df622c4eb3afc4092df9f29bbb94b0e8d639a3ec
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-05-12 17:16:01 +0700

    [Fresly] - commit from folder 11 video 4. Defining a Model

--- utils/database.js
+++ utils/database.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 
+// const sequelize = new Sequelize('database-name', 'username', 'password', {
 const sequelize = new Sequelize('node-complete', 'root', '', {
 	dialect: 'mysql',
 	host: 'localhost',
