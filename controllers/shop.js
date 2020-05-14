56d00d77fca3146561ce2d30973379fc98e7d4a1
Fresly L. Sipahutar <freslylumban@gmail.com>
2020-05-15 04:17:14 +0700

    [Fresly] - commit from folder 11 video 25. Resetting the Cart & Fetching and Outputting Orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -121,9 +121,11 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+	let fetchedCart;
 	req.user
 		.getCart()
 		.then((cart) => {
+			fetchedCart = cart;
 			return cart.getProducts();
 		})
 		.then((products) => {
@@ -140,16 +142,25 @@ exports.postOrder = (req, res, next) => {
 				.catch((err) => console.log(err));
 		})
 		.then((result) => {
+			return fetchedCart.setProducts(null);
+		})
+		.then((result) => {
 			res.redirect('/orders');
 		})
 		.catch((err) => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-	res.render('shop/orders', {
-		pageTitle: 'Your Orders',
-		path: '/orders',
-	});
+	req.user
+		.getOrders({ include: ['products'] })
+		.then((orders) => {
+			res.render('shop/orders', {
+				pageTitle: 'Your Orders',
+				path: '/orders',
+				orders: orders,
+			});
+		})
+		.catch((err) => console.log(err));
 };
 
 exports.getCheckout = (req, res, next) => {
